dad41831319620b75aeae8fe87f5b835072f165c
Savas Gioldasis <savvas.gioldasis@ing.com>
2025-12-22 12:31:31 +0200

    Add schedule for customers

--- dbx-project/src/dbx_project/defs/schedules.py
+++ dbx-project/src/dbx_project/defs/schedules.py
@@ -1,15 +1,19 @@
 
 import dagster as dg
 
-# Create a job specifically for the check
-freshness_check_job = dg.define_asset_job(
-    name="customers_freshness_check_job",
-    selection=dg.AssetSelection.checks_for_assets(dg.AssetKey(["target", "main", "customers"]))
+# Create a job to materialize raw_customers and its dependent assets
+raw_customers_job = dg.define_asset_job(
+    name="raw_customers_job",
+    selection=dg.AssetSelection.keys(
+        dg.AssetKey(["target", "main", "raw_customers"]),
+        dg.AssetKey(["target", "main", "stg_customers"]),
+        dg.AssetKey(["target", "main", "customers"])
+    )
 )
 
-# Schedule the job
-customers_freshness_schedule = dg.ScheduleDefinition(
-    name="customers_freshness_schedule",
-    job=freshness_check_job,
+# Schedule the job to run every minute
+raw_customers_schedule = dg.ScheduleDefinition(
+    name="raw_customers_schedule",
+    job=raw_customers_job,
     cron_schedule="* * * * *",
 )
